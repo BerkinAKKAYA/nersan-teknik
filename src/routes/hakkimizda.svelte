be61d317fec2374e4aa4c46aecba0ee78f914eef
Berkin Akkaya <berkin_akkaya@hotmail.com>
2020-06-18 21:22:00 +0300

    modified:   src/routes/hakkimizda.svelte

--- src/routes/hakkimizda.svelte
+++ src/routes/hakkimizda.svelte
@@ -1,4 +1,13 @@
 <style>
+	h1 { margin-bottom: 10px }
+	p {
+		font-size: 1.1em;
+		line-height: 1.8em;
+		text-align: justify;
+		text-indent: 2em;
+		margin-top: 20px;
+	}
+
 	@media (max-width: 700px)
 	{
 		h1 { text-align: center }
@@ -9,4 +18,11 @@
 	<title>Hakkımızda - Nersan Teknik</title>
 </svelte:head>
 
-<h1>hakkımızda</h1>
+<h1>HAKKIMIZDA</h1>
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vehicula gravida commodo. Sed eget iaculis urna. Phasellus eu mi sit amet nisi venenatis dapibus. Vestibulum vulputate nibh porta facilisis vulputate. Ut suscipit quam turpis, vitae hendrerit elit euismod sed. Mauris rhoncus vulputate hendrerit. Phasellus fringilla quam nec ligula maximus vulputate. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam id augue erat. Curabitur et odio lacus. Fusce enim libero, mattis rhoncus odio in, cursus vulputate lorem. Morbi fringilla velit ac nunc accumsan, eu sagittis augue elementum. Nam vitae nulla mollis, feugiat eros eget, ornare nisl.
+</p>
+<p>
+	Aenean porttitor est vitae velit bibendum congue. Suspendisse potenti. Quisque venenatis eros ac justo luctus, eget finibus nunc convallis. In viverra risus in libero tempor pellentesque. Donec eu semper leo. Etiam porttitor faucibus posuere. Nulla a mattis mauris. Suspendisse massa nisl, semper id accumsan id, finibus non elit. Maecenas eleifend nulla non urna tristique mattis. Integer hendrerit turpis vel metus volutpat, id cursus justo luctus. Fusce ut elit vitae purus elementum posuere. Donec at nulla bibendum, congue turpis in, ultrices dui. Integer sagittis hendrerit ultricies.
+</p>
+<p style="text-align:right"><b><i>Nersan Teknik</i></b></p>
